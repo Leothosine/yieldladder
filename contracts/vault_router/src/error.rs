c6867983a6522bc49b68cac23aa2dd82f7532275
soundsng <thesoundsng@gmail.com>
2026-07-16 16:58:09 +0100

    feat(vault_router): add DepositCapExceeded error variant

--- contracts/vault_router/src/error.rs
+++ contracts/vault_router/src/error.rs
@@ -5,7 +5,8 @@ use soroban_sdk::contracterror;
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 #[repr(u32)]
 pub enum VaultError {
-    InvalidTier     = 1,
-    BelowMinDeposit = 2,
-    LockNotExpired  = 3,
+    InvalidTier        = 1,
+    BelowMinDeposit    = 2,
+    LockNotExpired     = 3,
+    DepositCapExceeded = 5,
 }
